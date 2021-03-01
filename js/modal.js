656520ddf654a1eb0b3d232a31feb05aecc8df78
muwah9 <muwah9@gmail.com>
2021-03-01 14:15:02 +0300

    внес правки, где-то переделал)

---
+++ js/modal.js
@@ -0,0 +1,42 @@
+
+    const OpenPopup = document.querySelector(".button--contacts");
+    const Popup = document.querySelector(".popup");
+    const PopupClose = document.querySelector(".popup__close");
+    const FormName = document.querySelector(".feedback__name input");
+    const PopupForm = document.querySelector(".feedback");
+    const FormEmail = document.querySelector(".feedback__email input")
+
+    OpenPopup.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      Popup.classList.add("popup-show");
+      if (!FormName.value)
+        FormName.focus();
+
+    });
+
+    PopupClose.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      Popup.classList.remove("popup-show");
+      Popup.classList.remove("popup-error");
+    });
+
+    PopupForm.addEventListener("submit", function (evt) {
+      if (!FormName.value || !FormEmail.value) {
+        evt.preventDefault();
+        Popup.classList.remove("popup-error");
+        Popup.offsetWidth = Popup.offsetWidth;
+        Popup.classList.add("popup-error");
+
+      }
+    });
+
+    window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        if (Popup.classList.contains("popup-show")) {
+          evt.preventDefault();
+          Popup.classList.remove("popup-show");
+          Popup.classList.remove("popup-error");
+        }
+      }
+    });
+
